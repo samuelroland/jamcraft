1a95afcaf542fc36a1fd916211fef9068f1b4776
Samuel Roland <samuelroland@noreply.codeberg.org>
2024-12-12 00:33:09 +0100

    Add tailwindcss, init config for vite project, verify it's working in App.tsx

--- src/main/webui/src/App.tsx
+++ src/main/webui/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="italic text-sky-700 font-bold">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
