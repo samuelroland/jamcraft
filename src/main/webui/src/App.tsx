979710475840d57c8fe536d81256d3e58334d23a
Samuel Roland <samuelroland@noreply.codeberg.org>
2025-01-05 22:33:47 +0100

    tmp

--- src/main/webui/src/App.tsx
+++ src/main/webui/src/App.tsx
@@ -1,50 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport'
 import { HelloGrpcClient } from './hello.client'
+import { MouseServiceClient } from './mouse.client'
+const PROXY_BASE_URL = 'http://localhost:8081'
 
 function App() {
   const transport = new GrpcWebFetchTransport({
-    baseUrl: 'http://localhost:8081',
+    baseUrl: PROXY_BASE_URL,
     format: 'binary',
   })
-  const client = new HelloGrpcClient(transport)
-  const call = client.sayHello({
-    name: 'Jarod',
-  })
-  const headers = call.headers
-  console.log('got response headers: ', headers)
+  const [mousePosition, setMousePosition] = useState<{ x: number; y: number } | null>(null)
+  const [lastSentPosition, setLastSentPosition] = useState(JSON.parse(JSON.stringify(mousePosition)))
 
-  const response = call.response
-  console.log('got response message: ', response)
+  const onMouseMove = (event: MouseEvent) => {
+    setMousePosition({ x: event.clientX, y: event.clientY })
+    console.log('mouse moved ! new mouse position: ', mousePosition)
+  }
 
-  const status = call.status
-  console.log('got status: ', status)
+  const mouseClient = new MouseServiceClient(transport)
+  useEffect(() => {
+    mouseClient.getMouseUpdates({}).responses.onMessage((e) => console.log('Got new mouse position', e))
 
-  const trailers = call.trailers
-  console.log('got trailers: ', trailers)
-  const [count, setCount] = useState(0)
+    addEventListener('mousemove', onMouseMove)
 
+    const id = setInterval(() => {
+      if (mousePosition == null) {
+        console.log('is null')
+        return
+      }
+      if (lastSentPosition.x != 0 && mousePosition.x == lastSentPosition.x && mousePosition.y == lastSentPosition.y) {
+        console.log('skipped')
+        return
+      }
+      setLastSentPosition({ x: mousePosition.x, y: mousePosition.y })
+      console.log('Sent mouse position at ', mousePosition)
+      mouseClient.sendMousePosition({ userId: 12, x: mousePosition.x, y: mousePosition.y })
+    }, 1000)
+
+    return () => {
+      clearInterval(id)
+      removeEventListener('mousemove', onMouseMove)
+    }
+  }, [])
+
+  function sendMouse() {
+    mouseClient.sendMousePosition({ userId: 12, x: 123, y: 234 })
+  }
+
+  async function sayHello() {
+    const client = new HelloGrpcClient(transport)
+    const call = await client.sayHello({
+      name: 'Jarod',
+    })
+    const headers = call.headers
+    console.log('got response headers: ', headers)
+
+    const response = call.response
+    console.log('got response message: ', response)
+
+    const status = call.status
+    console.log('got status: ', status)
+
+    const trailers = call.trailers
+    console.log('got trailers: ', trailers)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="italic text-sky-700 font-bold">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <button onClick={() => sendMouse()}>Send mouse position</button>
     </>
   )
 }
