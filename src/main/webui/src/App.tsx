651efa84d1f33ce279a34f793973e6404589d706
Samuel Roland <samuelroland@noreply.codeberg.org>
2025-01-05 23:07:50 +0100

    Start of flying mouse POC

--- src/main/webui/src/App.tsx
+++ src/main/webui/src/App.tsx
@@ -1,73 +1,62 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport'
 import { HelloGrpcClient } from './hello.client'
 import { MouseServiceClient } from './mouse.client'
+import Test from './Test'
 const PROXY_BASE_URL = 'http://localhost:8081'
+const MIN_MOUSE_MSG = 300 // 300 ms -> 3 times/second
+// TODO: we should increase this number if the performance cost is acceptable
 
 function App() {
   const transport = new GrpcWebFetchTransport({
     baseUrl: PROXY_BASE_URL,
     format: 'binary',
   })
-  const [mousePosition, setMousePosition] = useState<{ x: number; y: number } | null>(null)
-  const [lastSentPosition, setLastSentPosition] = useState(JSON.parse(JSON.stringify(mousePosition)))
-
-  const onMouseMove = (event: MouseEvent) => {
-    setMousePosition({ x: event.clientX, y: event.clientY })
-    console.log('mouse moved ! new mouse position: ', mousePosition)
-  }
 
   const mouseClient = new MouseServiceClient(transport)
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  // With the help of Copilot, I found that this copy is absolutely necessary
+  // If we remove it, setInterval code will not be called when we continuously move the mouse
+  const latestMousePosition = useRef(mousePosition)
+  const previousMousePosition = useRef(mousePosition)
+
+  // Just save the new position each time it moves
+  const onMouseMove = (event: MouseEvent) => {
+    const newPosition = { x: event.clientX, y: event.clientY }
+    setMousePosition(newPosition)
+    latestMousePosition.current = newPosition // this copy is important
+  }
+
   useEffect(() => {
     mouseClient.getMouseUpdates({}).responses.onMessage((e) => console.log('Got new mouse position', e))
 
     addEventListener('mousemove', onMouseMove)
 
-    const id = setInterval(() => {
-      if (mousePosition == null) {
-        console.log('is null')
-        return
+    // Each MIN_MOUSE_MSG, if the position has changed, send the new one
+    const intervalId = setInterval(() => {
+      const { x: currentX, y: currentY } = latestMousePosition.current
+      const { x: previousX, y: previousY } = previousMousePosition.current
+
+      if (currentX !== previousX || currentY !== previousY) {
+        previousMousePosition.current = { x: currentX, y: currentY }
+        console.log('Sent mouse position at ', latestMousePosition.current)
+        mouseClient.sendMousePosition({ userId: 12, x: currentX, y: currentY })
       }
-      if (lastSentPosition.x != 0 && mousePosition.x == lastSentPosition.x && mousePosition.y == lastSentPosition.y) {
-        console.log('skipped')
-        return
-      }
-      setLastSentPosition({ x: mousePosition.x, y: mousePosition.y })
-      console.log('Sent mouse position at ', mousePosition)
-      mouseClient.sendMousePosition({ userId: 12, x: mousePosition.x, y: mousePosition.y })
-    }, 1000)
+    }, MIN_MOUSE_MSG)
 
     return () => {
-      clearInterval(id)
+      clearInterval(intervalId)
       removeEventListener('mousemove', onMouseMove)
     }
   }, [])
 
-  function sendMouse() {
-    mouseClient.sendMousePosition({ userId: 12, x: 123, y: 234 })
-  }
-
-  async function sayHello() {
-    const client = new HelloGrpcClient(transport)
-    const call = await client.sayHello({
-      name: 'Jarod',
-    })
-    const headers = call.headers
-    console.log('got response headers: ', headers)
-
-    const response = call.response
-    console.log('got response message: ', response)
-
-    const status = call.status
-    console.log('got status: ', status)
-
-    const trailers = call.trailers
-    console.log('got trailers: ', trailers)
-  }
   return (
     <>
-      <button onClick={() => sendMouse()}>Send mouse position</button>
+      <h1>POC mouses</h1>
+      Mouse position {mousePosition.x} {mousePosition.y} <br />
+      Sent position {previousMousePosition.current.x} {previousMousePosition.current.y}
     </>
   )
 }
