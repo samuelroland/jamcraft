da18431a69812714ebd1df1ff087763fc5736861
Samuel Roland <samuelroland@noreply.codeberg.org>
2025-01-10 14:46:54 +0100

    Add quarkus-rest and quarkus-jackson deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,13 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
-    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation(enforcedPlatform("$quarkusPlatformGroupId:$quarkusPlatformArtifactId:$quarkusPlatformVersion"))
     implementation("io.quarkus:quarkus-grpc")
     implementation("io.quarkus:quarkus-hibernate-orm")
     implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.5.1")
     implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-rest")
+    implementation("io.quarkus:quarkus-rest-jackson")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
     testImplementation("io.quarkus:quarkus-junit5")
 }
