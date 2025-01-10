eb01c52f4411dce7dce86104ab5ac31324ec20c7
DrCookie <timothee.vanhove@heig-vd.ch>
2025-01-10 14:16:12 +0100

    feat: sql import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,9 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+INSERT INTO samples (name, filepath, duration, created_at) VALUES
+('Funk Guitar Thing', 'aptbr__funk-guitar-thing.mp3', 12.042449, CURRENT_TIMESTAMP),
+('Lotus Guzheng Chops CM', 'f-r-a-g-i-l-e__lotus-guzheng-chops-cm.mp3', 8.803250, CURRENT_TIMESTAMP),
+('Piano Loops 120 BPM', 'josefpres__piano-loops-090-efect-3-octave-long-loop-120-bpm.mp3', 130.037551, CURRENT_TIMESTAMP),
+('Player Turn Start', 'mickleness__player-turn-start.mp3', 2.56, CURRENT_TIMESTAMP),
+('Electro House Loop', 'sakebeats__electro-house-loop.mp3', 7.549388, CURRENT_TIMESTAMP),
+('Quality Techno Beat', 'teacoma__quality-techno-beat.mp3', 7.706122, CURRENT_TIMESTAMP),
+('Impact Techno Drum F1 Loop', 'trubalesk__125bpm_impact_techno-drum-f1-loop-boom.mp3', 7.706122, CURRENT_TIMESTAMP),
+('Cool Breeze Sample 2', 'wave_former__cool-breeze-sample-2.mp3', 8.907755, CURRENT_TIMESTAMP);
