ebbf84ddd78b0bcc05ba25c93f7d0ef52a1506cd
Samuel Roland <samuelroland@noreply.codeberg.org>
2025-01-10 14:33:40 +0100

    Add tracks and sample_tracks data in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,20 @@ INSERT INTO samples (name, filepath, duration, created_at) VALUES
 ('Quality Techno Beat', 'teacoma__quality-techno-beat.mp3', 7.706122, CURRENT_TIMESTAMP),
 ('Impact Techno Drum F1 Loop', 'trubalesk__125bpm_impact_techno-drum-f1-loop-boom.mp3', 7.706122, CURRENT_TIMESTAMP),
 ('Cool Breeze Sample 2', 'wave_former__cool-breeze-sample-2.mp3', 8.907755, CURRENT_TIMESTAMP);
+
+INSERT INTO tracks (name, created_at, modified_at) VALUES
+    ('Track 1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+('Track 2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+('Track 3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+('Track 4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO sample_tracks (track_id, sample_id, start_time) VALUES
+(1, 1, 1.623)
+(1, 2, 3.623)
+(1, 7, 4.1)
+(2, 7, 4.1)
+(2, 8, 0.0)
+(2, 2, 4.1)
+(3, 2, 10.1)
+(4, 3, 0);
+
