1138b2abfddd4eae5a5be06ac56a197882f30318
Jarod <jarod.streckeisen@heig-vd.ch>
2025-01-17 12:15:34 +0100

    Add login dialog and Join call

--- src/main/webui/src/App.tsx
+++ src/main/webui/src/App.tsx
@@ -3,11 +3,14 @@ import './App.css'
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport'
 import { MouseServiceClient } from './grpc/mouse.client'
 import { MousePosition } from './grpc/mouse'
+import { UsersServiceClient} from "./grpc/users.client.ts";
 import TrackList from './components/TrackList'
 import { MIN_MOUSE_MSG_INTERVAL, PROXY_BASE_URL } from './constants'
 import Library from './components/Library'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {LoginDialog} from "./components/LoginDialog.tsx";
+import MouseCursor   from "./components/MouseCursor.tsx";
 
 function App() {
     const transport = useMemo(() => new GrpcWebFetchTransport({
@@ -16,12 +19,14 @@ function App() {
     }), [])
 
     const mouseClient = useMemo(() => new MouseServiceClient(transport), [transport])
+    const userClient = useMemo( ()=> new UsersServiceClient(transport),[transport])
 
     const [userId] = useState(Math.floor(Math.random() * 1000)) // random one for now
     // @ts-ignore
     const [otherMouses, setOtherMouses] = useState<Map<number, MousePosition>>(new Map())
 
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+    const [isLogged,setIsLogged] = useState(false)
     // With the help of Copilot, I found that this copy is absolutely necessary
     // If we remove it, setInterval code will not be called when we continuously move the mouse
     const latestMousePosition = useRef(mousePosition)
@@ -33,6 +38,15 @@ function App() {
         setMousePosition(newPosition)
         latestMousePosition.current = newPosition // this copy is important
     }
+    const handleLogin = (username) =>{
+        const promise = userClient.join({name:username})
+        promise.then((call)=>
+            console.log(call.response)
+        ).catch(error =>{
+            console.log(error)
+        })
+        setIsLogged(true)
+    }
 
     const handleUpdatedMousesPositions = (p: MousePosition) => {
         console.log('Got new position', p)
@@ -41,6 +55,7 @@ function App() {
         }
         setOtherMouses((ms) => {
             // Create a new Map to maintain immutability
+            console.log("mouses",ms)
             const newMap = new Map(ms)
             newMap.set(p.userId, p)
             return newMap
@@ -49,6 +64,7 @@ function App() {
     }
 
     useEffect(() => {
+        console.log("useEffect")
         mouseClient.getMouseUpdates({}).responses.onMessage((p) => handleUpdatedMousesPositions(p))
 
         addEventListener('mousemove', onMouseMove)
@@ -60,7 +76,7 @@ function App() {
 
             if (currentX !== previousX || currentY !== previousY) {
                 previousMousePosition.current = { x: currentX, y: currentY }
-                // console.log('Sent mouse position at ', latestMousePosition.current)
+                console.log('Sent mouse position at ', latestMousePosition.current)
                 mouseClient.sendMousePosition({ userId: userId, x: currentX, y: currentY })
             }
         }, MIN_MOUSE_MSG_INTERVAL)
@@ -73,6 +89,7 @@ function App() {
 
     return (
         <>
+            <LoginDialog is_logged={isLogged} login={handleLogin}></LoginDialog>
             <ToastContainer position="top-right" autoClose={4000} hideProgressBar={true} pauseOnHover={true}></ToastContainer>
             <div className="flex h-screen">
                 {/* Library on the left */}
@@ -85,6 +102,9 @@ function App() {
                     <TrackList/>
                 </div>
             </div>
+            {Array.from(otherMouses.values()).map((p) => (
+                <MouseCursor key={p.userId} p={p}></MouseCursor>
+            ))}
         </>
     )
 }
---
+++ src/main/webui/src/components/LoginDialog.tsx
@@ -0,0 +1,47 @@
+import { Button } from "./ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "./ui/dialog"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+import {useState} from "react";
+
+
+export function LoginDialog({is_logged,login}) {
+
+    const [username,setUsername] = useState("")
+
+    const handleChange = (e) =>{
+        setUsername(e.target.value)
+    }
+
+    return (
+        <Dialog open={!is_logged}>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Welcome to Jamcraft</DialogTitle>
+                    <DialogDescription>
+                        Please provide a username to access the app
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input id="username"  className="col-span-3" onChange={(e)=>handleChange(e)} />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button onClick={()=>login(username)}>Login</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
