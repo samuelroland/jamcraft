880da557bdb554901c792597f710fd094e44240b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-01-23 00:22:20 +0100

    Refactor to not hardcode proxy domain only the port

--- src/main/webui/src/constants.ts
+++ src/main/webui/src/constants.ts
@@ -1,4 +1,4 @@
-export const PROXY_BASE_URL = 'http://localhost:8081';
+export const PROXY_PORT = 8081;
 export const MIN_MOUSE_MSG_INTERVAL = 200; // 200 ms -> 5/second
 export const MIN_SAMPLE_POSITION_MSG_INTERVAL = 80; // 200 ms -> 5/second
 export const LIBRARY_COLORS: string[] = ['#e83f3f', '#23852f', '#3b70be', '#e57c2f', '#5c218a'];
--- src/main/webui/src/lib/utils.ts
+++ src/main/webui/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { PROXY_BASE_URL } from '../constants';
+import { PROXY_PORT } from '../constants';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -12,7 +12,7 @@ let transportInstance: GrpcWebFetchTransport | null = null;
 export function getGrpcTransport() {
     if (!transportInstance) {
         transportInstance = new GrpcWebFetchTransport({
-            baseUrl: PROXY_BASE_URL,
+            baseUrl: 'http://' + window.location.hostname + ':' + PROXY_PORT,
             format: 'binary',
             // timeout: 2000, // no default timeout, each request has to decide or use the default...
         });
