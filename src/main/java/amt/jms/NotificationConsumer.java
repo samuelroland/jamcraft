914ff5cdb839e711b957e4dd304b1cf92892534e
DrCookie <timothee.vanhove@heig-vd.ch>
2025-01-26 13:04:57 +0100

    doc: javadoc JMS classes

--- src/main/java/amt/jms/NotificationConsumer.java
+++ src/main/java/amt/jms/NotificationConsumer.java
@@ -11,6 +11,13 @@ import jakarta.jms.JMSContext;
 import org.jboss.logging.Logger;
 import io.smallrye.mutiny.Multi;
 
+/**
+ * A JMS consumer that listens for user notifications on the "notifications" queue.
+ * This consumer receives notifications as {@link UserDTO} objects and provides a reactive stream of notifications
+ * for other components to subscribe to using Mutiny's {@link Multi}.
+ *
+ * @author Yanis Ouadahi, Samuel Roland, Jarod Streckeisen, Timothée Van Hove
+ */
 @ApplicationScoped
 public class NotificationConsumer {
 
@@ -22,12 +29,21 @@ public class NotificationConsumer {
 
     private static final Logger LOG = Logger.getLogger(NotificationConsumer.class);
 
+    /**
+     * Initializes the JMS context and consumer for the "notifications" queue.
+     * This method is called automatically after the bean is constructed.
+     */
     @PostConstruct
     public void start() {
         context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE);
         consumer = context.createConsumer(context.createQueue("notifications"));
     }
 
+    /**
+     * Provides a reactive stream of {@link UserDTO} notifications.
+     *
+     * @return a {@link Multi} stream of {@link UserDTO} notifications.
+     */
     public Multi<UserDTO> getNotificationStream() {
         // Create a Multi stream from JMS consumer
         return Multi.createFrom().emitter(emitter -> {
@@ -46,6 +62,10 @@ public class NotificationConsumer {
         });
     }
 
+    /**
+     * Closes the JMS context and cleans up resources.
+     * This method is called automatically before the bean is destroyed.
+     */
     @PreDestroy
     public void stop() {
         if (context != null) {
--- src/main/java/amt/jms/NotificationProducer.java
+++ src/main/java/amt/jms/NotificationProducer.java
@@ -6,12 +6,24 @@ import jakarta.inject.Inject;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSContext;
 
+/**
+ * A JMS producer that sends user notifications to the "notifications" queue.
+ * This producer allows other components to broadcast notifications by sending
+ * {@link UserDTO} objects to the JMS queue.
+ *
+ * @author Yanis Ouadahi, Samuel Roland, Jarod Streckeisen, Timothée Van Hove
+ */
 @ApplicationScoped
 public class NotificationProducer {
 
     @Inject
     ConnectionFactory connectionFactory;
 
+    /**
+     * Sends a {@link UserDTO} notification to the "notifications" queue.
+     *
+     * @param user the {@link UserDTO} object containing user information to be sent.
+     */
     public void sendNotification(UserDTO user) {
         try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
             context.createProducer().send(context.createQueue("notifications"), user);
