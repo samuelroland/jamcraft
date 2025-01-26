7ff70fbddcc89694731832f52fe6806ce2e8ae85
DrCookie <timothee.vanhove@heig-vd.ch>
2025-01-26 12:32:34 +0100

    rollback jms changes

--- src/main/java/amt/jms/NotificationConsumer.java
+++ src/main/java/amt/jms/NotificationConsumer.java
@@ -8,7 +8,6 @@ import jakarta.inject.Inject;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSConsumer;
 import jakarta.jms.JMSContext;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import io.smallrye.mutiny.Multi;
 
@@ -18,10 +17,6 @@ public class NotificationConsumer {
     @Inject
     ConnectionFactory connectionFactory;
 
-    @ConfigProperty(name = "notification.queue.name")
-    String queueName;
-
-
     private JMSContext context;
     private JMSConsumer consumer;
 
@@ -30,29 +25,24 @@ public class NotificationConsumer {
     @PostConstruct
     public void start() {
         context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE);
-        consumer = context.createConsumer(context.createQueue(queueName));
-        LOG.info(getClass().getSimpleName() + " started. Listening to queue: " + queueName);
+        consumer = context.createConsumer(context.createQueue("notifications"));
     }
 
     public Multi<UserDTO> getNotificationStream() {
+        // Create a Multi stream from JMS consumer
         return Multi.createFrom().emitter(emitter -> {
-            Runnable task = () -> {
+            new Thread(() -> {
                 try {
                     while (true) {
                         UserDTO user = consumer.receiveBody(UserDTO.class);
-                        if (user != null) {
-                            LOG.info("Received notification: " + user);
-                            emitter.emit(user);
-                        }
+                        LOG.info("Received notification: " + user);
+                        emitter.emit(user);
                     }
                 } catch (Exception e) {
                     LOG.error("Error receiving message", e);
                     emitter.fail(e);
                 }
-            };
-            Thread thread = new Thread(task, "NotificationConsumerThread");
-            thread.setDaemon(true);
-            thread.start();
+            }).start();
         });
     }
 
@@ -60,8 +50,6 @@ public class NotificationConsumer {
     public void stop() {
         if (context != null) {
             context.close();
-            LOG.info("NotificationConsumer stopped.");
         }
     }
 }
-
--- src/main/java/amt/jms/NotificationProducer.java
+++ src/main/java/amt/jms/NotificationProducer.java
@@ -5,8 +5,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSContext;
-import org.jboss.logging.Logger;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class NotificationProducer {
@@ -14,17 +12,9 @@ public class NotificationProducer {
     @Inject
     ConnectionFactory connectionFactory;
 
-    @ConfigProperty(name = "notification.queue.name")
-    String queueName;
-
-    private static final Logger LOG = Logger.getLogger(NotificationProducer.class);
-
     public void sendNotification(UserDTO user) {
         try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
-            context.createProducer().send(context.createQueue(queueName), user);
-            LOG.info("Notification sent to queue '" + queueName + "' for user: " + user);
-        } catch (Exception e) {
-            LOG.error("Failed to send notification to queue '" + queueName + "' for user: " + user, e);
+            context.createProducer().send(context.createQueue("notifications"), user);
         }
     }
 }
